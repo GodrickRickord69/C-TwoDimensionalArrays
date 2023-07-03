670fbf9da79c9ef63d6012c9d0e8b8de3b7e5fc1
Nikita <varashilov125@gmail.com>
2023-07-03 19:00:49 +0300

    Task3 среднее арифметическое двумер. массива

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,3 +7,46 @@
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 * Сумма всех цифр / их количество(это количество ваших строк)*/
 
+int[,] array = GetArray(4, 4, 0, 10);
+PrintArray(array);
+PrintArithmeticMean(array);
+
+
+int[,] GetArray(int m, int n, int MinValue, int MaxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(MinValue, MaxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintArithmeticMean(int[,] array)
+{
+    double ArithmeticMean;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        ArithmeticMean = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            ArithmeticMean += array[j, i];
+        }
+        ArithmeticMean = ArithmeticMean / array.GetLength(0);
+        Console.WriteLine($"Среднее арифметическое столбца {i + 1}: {ArithmeticMean}");
+    }
+}

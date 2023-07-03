a1a307a161c73ee4782e04a8062c360755fa9d7d
Nikita <varashilov125@gmail.com>
2023-07-03 17:15:42 +0300

    Task1 двумер. массив вещественных чисел

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,37 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9*/
+
+Console.Write("Введите количество сторок: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, double MinValue, double MaxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * (MaxValue - MinValue) + MinValue;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round((decimal)inArray[i, j], 2)} ");
+        }
+        Console.WriteLine();
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,3 +6,38 @@
 8 4 2 4
 i = 1, j = 2 -> 2
 i = 4, j = 2 -> такого элемента не существует*/
+
+
+Console.Write("Введите количество сторок: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int MinValue, int MaxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(MinValue, MaxValue);
+        }
+        return result;
+    };
+}
+int PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}");
+        }
+        Console.WriteLine();
+    }
+    return inArray;
+}
